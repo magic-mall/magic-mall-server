e4a3aa848fec93371fd0cf7f066de597f37f6577
songdaochuanshu <songdaochuanshu@gmail.com>
2022-03-27 03:09:31 +0800

    👌 update sql-doc

--- sql-doc-generate.js
+++ sql-doc-generate.js
@@ -1,8 +1,5 @@
-const child = require("child_process");
-const path = require("path");
-const os = require("os");
 const dotenv = require("dotenv");
-const fs = require("fs");
+const { MySQLMarkdown } = require("mysql-markdown");
 
 dotenv.config();
 
@@ -14,40 +11,14 @@ const {
   DB_PASSWORD,
 } = process.env;
 
-const platform = os.platform();
-let filePath = (name) => path.join(__dirname, "./scripts", name);
-let fileName = "";
+let mySQLMarkdown = new MySQLMarkdown({
+  host: HOST,
+  port: PORT,
+  user: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DATABASE,
+  output: "./",
+  fileName: "sql-doc",
+});
 
-switch (platform) {
-  case "win32":
-    fileName = "mysql_markdown_win.exe";
-    filePath = filePath(fileName);
-    break;
-  case "linux":
-    fileName = "mysql_markdown_unix";
-    filePath = filePath(fileName);
-    break;
-  case "mac":
-    fileName = "mysql_markdown_mac";
-    filePath = filePath(fileName);
-    break;
-  default:
-    console.log("Unsupported platform");
-    break;
-}
-
-const magicEggFile = path.join(__dirname, "./magic-egg.md");
-const magicEggFileExists = fs.existsSync(magicEggFile);
-if (!magicEggFileExists) {
-  fs.writeFileSync(magicEggFile, "");
-}else{
-  fs.unlinkSync(magicEggFile);
-  fs.writeFileSync(magicEggFile, "");
-}
-
-child.exec(
-  `${filePath} -u ${DB_USERNAME} -p ${DB_PASSWORD} -d ${DATABASE} -P ${PORT} -h ${HOST} -o ${magicEggFile}`,
-  (err, stdout, stderr) => {
-    console.log(err, stdout, stderr);
-  }
-);
+mySQLMarkdown.build();
